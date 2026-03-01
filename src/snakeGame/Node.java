45de4f202f15d28a874e6bed323397c267980881
Finn <finn@java.com>
2026-03-01 15:18:46 +0800

    暂时完成了生成窗口和节点的任务，稍后继续

---
+++ src/snakeGame/Node.java
@@ -0,0 +1,34 @@
+package snakeGame;
+//节点类，每一个节点就是一个小方块
+public class Node {
+    private int x;//横坐标
+    private int y;//纵坐标
+
+    public Node() {
+    }
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    //构造方法用于随机生成一个节点来代表食物
+    public void randomNode(){
+
+    }
+}
